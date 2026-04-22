270ff865e640e2713f88ef5915114aa3d8cf4e9a
DansLean <eudaniellcsilva@gmail.com>
2026-04-22 11:21:18 -0300

    Initial Commit

---
+++ DressUpGame/DressUpGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DressUpGameApp.swift
+//  DressUpGame
+//
+//  Created by User on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct DressUpGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
