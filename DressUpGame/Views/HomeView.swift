52aa8367b1dc37101bfc2fe4c47fde01a1ee9962
DansLean <eudaniellcsilva@gmail.com>
2026-04-24 08:26:56 -0300

    feat: add background to home

--- DressUpGame/Views/HomeView.swift
+++ DressUpGame/Views/HomeView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("HomeBackground")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+            HStack {
+                Text("Hello, World!")
+            }
         }
-        .padding()
     }
 }
 
