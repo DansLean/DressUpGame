bf134a7fe5e20e308a2b4c84dcedbc9848c82ccd
DansLean <eudaniellcsilva@gmail.com>
2026-04-28 15:19:38 -0300

    fix: adjust doll stack

--- DressUpGame/Components/Doll.swift
+++ DressUpGame/Components/Doll.swift
@@ -19,24 +19,24 @@ struct Doll: View {
                 Image("Cabelo1")   // Cabelo
                     .resizable()
                     .scaledToFit()
-                    .frame(width: geometry.size.width / 3, height: geometry.size.height / 1.25)
-                    .position(x: geometry.size.width / 2, y: geometry.size.height / 2 - 220)
+                    .frame(width: geometry.size.width / 3, height: geometry.size.height / 5.15)
+                    .position(x: geometry.size.width / 2, y: geometry.size.height / 2 - 165)
                 Image("")   // Acessórios
                 Image("Shoes1")   // Calçado
                     .resizable()
                     .scaledToFit()
-                    .frame(width: geometry.size.width / 2.6, height: geometry.size.height / 1.25)
-                    .position(x: geometry.size.width / 2, y: geometry.size.height / 2 + 275)
+                    .frame(width: geometry.size.width / 2.6, height: geometry.size.height / 5.7)
+                    .position(x: geometry.size.width / 2, y: geometry.size.height / 2 + 230)
                 Image("Bottom1")   // Bottom
                     .resizable()
                     .scaledToFit()
-                    .frame(width: geometry.size.width / 2.8, height: geometry.size.height / 1.25)
+                    .frame(width: geometry.size.width / 2.8, height: geometry.size.height / 2.9)
                     .position(x: geometry.size.width / 2, y: geometry.size.height / 2 + 100)
                 Image("Top1")   // Top
                     .resizable()
                     .scaledToFit()
-                    .frame(width: geometry.size.width / 2.8, height: geometry.size.height / 1.25)
-                    .position(x: geometry.size.width / 2, y: geometry.size.height / 2 - 55)
+                    .frame(width: geometry.size.width / 2.8, height: geometry.size.height / 4.3)
+                    .position(x: geometry.size.width / 2, y: geometry.size.height / 2 - 32)
             }
         }
     }
