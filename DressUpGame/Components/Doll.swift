d2cdb313995d4cdd62b42ba7aad50c8e426dabdf
DansLean <eudaniellcsilva@gmail.com>
2026-05-06 09:38:50 -0300

    fix: hair position offset

--- DressUpGame/Components/Doll.swift
+++ DressUpGame/Components/Doll.swift
@@ -67,6 +67,7 @@ struct DollView: View {
                 Image("Cabelo\(doll.hair)")   // Cabelo
                     .resizable()
                     .scaledToFit()
+            .offset(x: -5, y: -17)
 //                    .frame(width: geometry.size.width / 3, height: geometry.size.height)
 //                    .position(x: geometry.size.width / 2 - 2, y: geometry.size.height / 2 + 16)
                 
