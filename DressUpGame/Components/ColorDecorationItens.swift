f93019274ce74ae7bdbb83a85b099074c5aec19f
DansLean <eudaniellcsilva@gmail.com>
2026-06-05 11:40:56 -0300

    feat: adicionando correção para boa exibição e funcionamento do componente

--- DressUpGame/Components/ColorDecorationItens.swift
+++ DressUpGame/Components/ColorDecorationItens.swift
@@ -7,7 +7,10 @@
 
 import SwiftUI
 
+
 struct ColorDecorationItens: View {
+    
+    var tap: (Color) -> Void
     
     @State var colors = [
         AssetColor(color: .grayColorNew),
@@ -23,30 +26,23 @@ struct ColorDecorationItens: View {
     let sizeScreen: CGFloat = UIScreen.main.bounds.width + UIScreen.main.bounds.height / UIScreen.main.bounds.width
     
     var body: some View {
-//        ScrollView(.horizontal) {
-        ViewThatFits {
+        ScrollView(.horizontal) {
             LazyHGrid(rows: Array(repeating: GridItem(.flexible()), count: 1)) {
                 ForEach(colors, id: \.self) { colorOption in
                     Rectangle()
                         .foregroundColor(.white)
                         .overlay {
                             Circle()
-                                .foregroundColor(Color(uiColor: colorOption.color))
+                                .foregroundColor(colorOption.color)
+                                .onTapGesture {
+                                    tap(colorOption.color)
+                                }
                         }
                         .aspectRatio(0.00173 * sizeScreen, contentMode: .fit)
                 }
             }
+            .padding(.horizontal, 10)
+            .background(.white)
         }
-        .containerRelativeFrame([.horizontal, .vertical]) { length, axis in
-            if axis == .vertical {
-                return length * 0.1
-            } else {
-                return length
-            }
-        }
-        .background(.white)
-        
-//        }
     }
-    
 }
